3febe5f7e5e264fa42f6fccb7b4599c64ff9d135
Admin <admin@Mac-Admin.local>
2020-10-02 10:13:18 +0300

    libasm

--- ft_read.s
+++ ft_read.s
@@ -1,8 +1,17 @@
+				extern	___error
 				section .text
 				global _ft_read
 
 _ft_read:
 				mov		rax, 0x2000003
 				syscall
+				call	___error
+				ret
+.error:
+				push	rax
+				call	___error
+				pop		rdx
+				mov		[rax], edx
+				mov		rax, -1
 				ret
 
--- ft_write.s
+++ ft_write.s
@@ -1,7 +1,16 @@
+				extern	___error
 				section .text
 				global _ft_write
 
 _ft_write:
-				mov		rax, 0x200004
+				mov		rax, 0x2000004
 				syscall
+				jc		.error
+				ret
+.error:
+				push	rax
+				call	___error
+				pop		rdx
+				mov		[rax], edx
+				mov		rax, -1
 				ret
