1c962cc483778bf3b8f5675788747468d27a4974
Admin <admin@Mac-Admin.local>
2020-10-05 21:22:26 +0300

    libasm

--- ft_read.s
+++ ft_read.s
@@ -3,15 +3,15 @@
 				global _ft_read
 
 _ft_read:
-				mov		rax, 0x2000003
-				syscall
-				jc		.error
-				ret
+				mov		rax, 0x2000003		;	sys_read call identifier
+				syscall						;	call sys_read function
+				jc		.error				;	jump if carry flag is set
+				ret							;	return rax
 .error:
-				push	rax
-				call	___error
-				pop		rdx
-				mov		[rax], edx
-				mov		rax, -1
-				ret
+				push	rax					;	push rax value to top of stack to save errno value
+				call	___error			;	call error to get error address
+				pop		rdx					;	retrieve value from top of stack to rdx
+				mov		[rax], edx			;	move errno value to errno address
+				mov		rax, -1				;	rax = -1
+				ret							;	return rax
 
--- ft_write.s
+++ ft_write.s
@@ -3,14 +3,14 @@
 				global _ft_write
 
 _ft_write:
-				mov		rax, 0x2000004
-				syscall
-				jc		.error
-				ret
+				mov		rax, 0x2000004		;	sys_write call identifier
+				syscall						;	call sys_write function
+				jc		.error				;	jump if carry flag set 
+				ret							;	return rax
 .error:
-				push	rax
-				call	___error
-				pop		rdx
-				mov		[rax], edx
-				mov		rax, -1
-				ret
+				push	rax					;	push rax value to top of stack to save errno value
+				call	___error			;	call error to get error address
+				pop		rdx					;	retrieve value from top of stack to rdx
+				mov		[rax], edx			;	move errno value to errno address
+				mov		rax, -1				;	rax = -1
+				ret							;	return rax
